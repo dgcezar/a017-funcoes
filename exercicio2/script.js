97a1644e49b7c9e99b1ab167d86f07d0d492b894
dgcezar <diego.cezar13@gmail.com>
2022-09-14 12:22:47 -0300

    att1 - exercício 2 feito

---
+++ exercicio2/script.js
@@ -0,0 +1,43 @@
+/*
+a)
+
+const userNumeros = (num1, num2) => {
+    let soma = num1 + num2;
+    return soma
+}
+
+console.log(userNumeros(5,5));
+*/
+
+/*
+b)
+
+const userNumeros = (num1, num2) => {
+    if (num1 >= num2) {
+        console.log("O primeiro número É maior ou igual ao segundo.");
+    } else {
+        console.log("O primeiro número NÃO é maior ou igual ao segundo.");
+    }
+}
+
+userNumeros(4,5);
+*/ 
+/*
+c)
+
+const numeroPar = (numero) => {
+    numero % 2 === 0 ? console.log("O número é par.") : console.log("O número não é par.");
+}
+
+numeroPar(10); 
+*/
+
+/*
+d)
+
+const userMensagem = (mensagem) => {
+    console.log(`Este é o tamanho da mensagem: ${mensagem.length}, e essa é a mensagem em letras maiúsculas: ${mensagem.toUpperCase()}`);
+}
+
+userMensagem("A aula de hoje é sobre funções");
+*/
