907115d4cfffa44955ded3978d6f56b64f107396
dgcezar <diego.cezar13@gmail.com>
2022-09-14 12:01:32 -0300

    Primeiro - exercício 1 feito

---
+++ exercicio1/script.js
@@ -0,0 +1,35 @@
+/*
+a)
+
+function olaNome(nome) {
+    console.log(`Ola ${nome}`);
+}
+
+olaNome("Diego");
+*/
+
+/*
+b)
+
+const tabuada = function() {
+    let seis = [1*6,2*6,3*6,4*6,5*6,6*6,7*6,8*6,9*6,10*6]
+    console.log(seis);
+}
+
+tabuada();
+*/
+
+/*c)
+
+const olaNome = (nome) => {
+    console.log(`Ola ${nome}.`);
+}
+
+olaNome("Diego");
+
+const tabuada = () => {
+    let seis = [1*6,2*6,3*6,4*6,5*6,6*6,7*6,8*6,9*6,10*6];
+    console.log(seis);
+}
+
+tabuada();*/
